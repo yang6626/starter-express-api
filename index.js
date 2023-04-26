febdb0c287d13ae3e427cc0b4af587694d4daa75
yang6626 <305020042@qq.com>
2023-04-26 15:36:47 +0800

    1

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-const express = require('express')
-const app = express();
-app.use(express.json());
-app.all('/', async (req, res) => {
-    try {
-      const data = await fetch(
-        `https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/getMe`
-      );
-      const json = await data.json();
-      res.send(json);
-    } catch (error) {
-      console.log(error);
-    }
-})
-app.listen(process.env.PORT || 3000)
+// const express = require('express')
+// const app = express();
+// app.use(express.json());
+// app.get('/', async (req, res) => {
+//     try {
+//       const data = await fetch(
+//         `https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/getMe`
+//       );
+//       const json = await data.json();
+//       res.send(json);
+//     } catch (error) {
+//       console.log(error);
+//     }
+// })
+// app.listen(process.env.PORT || 3000)
+import { Telegraf } from "telegraf";
+const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
+
+bot.command("say", async (ctx) => {
+  console.log(ctx);
+  ctx.reply("say command");
+  // // Explicit usage
+  // await ctx.telegram.sendMessage();
+
+  // // Using context shortcut
+  // await ctx.leaveChat();
+});
+
+bot.start((ctx) => ctx.reply("Welcome"));
+bot.launch();
+
